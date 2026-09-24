3c2e2dde6a169224d154bade62c5d9306a8cfe73
samubj <bjsamukalavathy@gmail.com>
2026-09-24 18:04:18 +0530

    Final Commit

--- src/DataProvider.jsx
+++ src/DataProvider.jsx
@@ -8,7 +8,7 @@ const DataProvider = ({ children }) => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await fetch("https://dummyjson.com/products?limit=12")
+                const response = await fetch("https://dummyjson.com/products/category/smartphones")
                 if (!response.ok) {
                     throw new Error("Failed to fetch products")
                 }
